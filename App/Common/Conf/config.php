1e19af397011e0d8276dd50109976ba99d4cc0ef
gtzhong <845272922@qq.com>
2017-02-08 22:24:44 +0800

    首页展示

--- App/Common/Conf/config.php
+++ App/Common/Conf/config.php
@@ -1,4 +1,15 @@
 <?php
+$website = http_type().$_SERVER['SERVER_NAME'];
 return array(
-	//'配置项'=>'配置值'
+    'TMPL_TEMPLATE_SUFFIX'=>'.phtml',
+    //让页面显示追踪日志信息
+    'SHOW_PAGE_TRACE'   => true,
+    //不区分大小写
+    'URL_CASE_INSENSITIVE' => false,
+
+    //前台样式配置
+    'f-css'=> $website.'/assets/home/css',
+    'f-js'=> $website. '/assets/home/js',
+    'f-img'=>$website. '/assets/home/images',
+
 );
